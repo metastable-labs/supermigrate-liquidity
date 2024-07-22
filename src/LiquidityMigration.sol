d356ea616ae3a6ee65eb512c2625a4418fddb2bc
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-07-22 18:42:27 +0100

    update design specs

--- src/LiquidityMigration.sol
+++ src/LiquidityMigration.sol
@@ -1,1 +1,5 @@
+// SPDX-License-Identifier: GNU
+pragma solidity ^0.8.19;
 
+import {ILayerZeroEndpointV2, MessagingFee, MessagingReceipt, Origin} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
+import {ILayerZeroComposer} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroComposer.sol";
