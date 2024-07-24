f422d88283a9a99dc50a3970bf677207223e8206
Njoku Emmanuel <kalunjoku123@gmail.com>
2024-07-24 20:37:18 +0100

    chore: add method to check if pool is v3

--- src/LiquidityMigration.sol
+++ src/LiquidityMigration.sol
@@ -1,5 +1,147 @@
 // SPDX-License-Identifier: GNU
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.20;
 
-import {ILayerZeroEndpointV2, MessagingFee, MessagingReceipt, Origin} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
-import {ILayerZeroComposer} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroComposer.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {OApp, MessagingFee, Origin} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OApp.sol";
+import {MessagingReceipt} from "@layerzerolabs/lz-evm-oapp-v2/contracts/oapp/OAppSender.sol";
+
+interface IUniswapV2Factory {
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+}
+
+interface IUniswapV2Router02 {
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+}
+
+interface IUniswapV3Factory {
+    function getPool(address tokenA, address tokenB, uint24 fee) external view returns (address pool);
+}
+
+interface INonfungiblePositionManager {
+    struct DecreaseLiquidityParams {
+        uint256 tokenId;
+        uint128 liquidity;
+        uint256 amount0Min;
+        uint256 amount1Min;
+        uint256 deadline;
+    }
+
+    function decreaseLiquidity(DecreaseLiquidityParams calldata params)
+        external
+        payable
+        returns (uint256 amount0, uint256 amount1);
+    function positions(uint256 tokenId)
+        external
+        view
+        returns (
+            uint96 nonce,
+            address operator,
+            address token0,
+            address token1,
+            uint24 fee,
+            int24 tickLower,
+            int24 tickUpper,
+            uint128 liquidity,
+            uint256 feeGrowthInside0LastX128,
+            uint256 feeGrowthInside1LastX128,
+            uint128 tokensOwed0,
+            uint128 tokensOwed1
+        );
+}
+
+interface StandardBridge {
+    event ERC20BridgeFinalized(
+        address indexed localToken,
+        address indexed remoteToken,
+        address indexed from,
+        address to,
+        uint256 amount,
+        bytes extraData
+    );
+    event ERC20BridgeInitiated(
+        address indexed localToken,
+        address indexed remoteToken,
+        address indexed from,
+        address to,
+        uint256 amount,
+        bytes extraData
+    );
+    event ETHBridgeFinalized(address indexed from, address indexed to, uint256 amount, bytes extraData);
+    event ETHBridgeInitiated(address indexed from, address indexed to, uint256 amount, bytes extraData);
+
+    function bridgeERC20(
+        address _localToken,
+        address _remoteToken,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes memory _extraData
+    ) external;
+    function bridgeERC20To(
+        address _localToken,
+        address _remoteToken,
+        address _to,
+        uint256 _amount,
+        uint32 _minGasLimit,
+        bytes memory _extraData
+    ) external;
+    function bridgeETH(uint32 _minGasLimit, bytes memory _extraData) external payable;
+    function bridgeETHTo(address _to, uint32 _minGasLimit, bytes memory _extraData) external payable;
+    function deposits(address, address) external view returns (uint256);
+    function finalizeBridgeERC20(
+        address _localToken,
+        address _remoteToken,
+        address _from,
+        address _to,
+        uint256 _amount,
+        bytes memory _extraData
+    ) external;
+    function finalizeBridgeETH(address _from, address _to, uint256 _amount, bytes memory _extraData) external payable;
+    function messenger() external view returns (address);
+    function OTHER_BRIDGE() external view returns (address);
+}
+
+contract LiquidityMigration is OApp {
+    IUniswapV2Factory public immutable uniswapV2Factory;
+    IUniswapV2Router02 public immutable uniswapV2Router;
+    IUniswapV3Factory public immutable uniswapV3Factory;
+    INonfungiblePositionManager public immutable nonfungiblePositionManager;
+    StandardBridge public immutable l1StandardBridge;
+
+    event LiquidityRemoved(address tokenA, address tokenB, uint256 amountA, uint256 amountB);
+    event TokensBridged(address token, uint256 amount);
+
+    constructor(
+        address _endpoint,
+        address _delegate,
+        address _uniswapV2Factory,
+        address _uniswapV2Router,
+        address _uniswapV3Factory,
+        address _nonfungiblePositionManager,
+        address _l1StandardBridge
+    ) OApp(_endpoint, _delegate) Ownable(_delegate) {
+        uniswapV2Factory = IUniswapV2Factory(_uniswapV2Factory);
+        uniswapV2Router = IUniswapV2Router02(_uniswapV2Router);
+        uniswapV3Factory = IUniswapV3Factory(_uniswapV3Factory);
+        nonfungiblePositionManager = INonfungiblePositionManager(_nonfungiblePositionManager);
+        l1StandardBridge = StandardBridge(_l1StandardBridge);
+    }
+
+    function isV3Pool(address tokenA, address tokenB) public view returns (bool) {
+        uint16[3] memory fees = [500, 3000, 10_000];
+        for (uint256 i = 0; i < fees.length; i++) {
+            if (uniswapV3Factory.getPool(tokenA, tokenB, fees[i]) != address(0)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
