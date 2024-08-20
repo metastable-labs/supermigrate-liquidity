d60641b18e851ba4802419f260864d5179c68704
Juan <0xjuaan@gmail.com>
2024-08-20 15:57:58 +0800

    removed unnecessary interface

--- src/modules/L2LiquidityManager.sol
+++ src/modules/L2LiquidityManager.sol
@@ -591,17 +591,3 @@ interface ISwapRouterV3 {
     /// @return amountOut The amount of the received token
     function exactInputSingle(ExactInputSingleParams calldata params) external payable returns (uint256 amountOut);
 }
-
-
-/// @title Pool state that never changes
-/// @notice These parameters are not defined as immutable (due to proxy pattern) but are effectively immutable.
-/// @notice i.e., the methods will always return the same values
-interface ICLPoolConstants {
-
-    /// @notice The pool tick spacing
-    /// @dev Ticks can only be used at multiples of this value, minimum of 1 and always positive
-    /// e.g.: a tickSpacing of 3 means ticks can be initialized every 3rd tick, i.e., ..., -6, -3, 0, 3, 6, ...
-    /// This value is an int24 to avoid casting even though it is always positive.
-    /// @return The tick spacing
-    function tickSpacing() external view returns (int24);
-}
